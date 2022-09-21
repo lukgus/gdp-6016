e8f800c50a97be083479da5322426016ac145442
Lukas Gustafson <l_gustafson@fanshaweonline.ca>
2022-09-21 19:28:27 -0400

    Added comments.

--- NetworkExamples/NetworkExamples/Buffer.h
+++ NetworkExamples/NetworkExamples/Buffer.h
@@ -8,6 +8,13 @@ public:
 
 	void WriteInt32LE(std::size_t index, int32_t value);
 	void WriteInt32LE(int32_t value);
+	/*
+	 * [m_WriteIndex++] <-- uses the value before incrementing it
+	 * 
+	 * [++m_WriteIndex] <-- Increments the value before using it
+	 * m_WriteIndex += 4;
+	 **/
+
 	uint32_t ReadUInt32LE(std::size_t index);
 	uint32_t ReadUInt32LE();
 
@@ -16,6 +23,6 @@ private:
 	std::vector<uint8_t> m_Buffer;
 
 	// initialize these at 0
-	int m_WriteBuffer;
-	int m_ReadBuffer;
+	int m_WriteIndex;
+	int m_ReadIndex;
 };
