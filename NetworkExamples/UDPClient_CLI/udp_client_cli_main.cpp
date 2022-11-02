3c1d29a5f02828750154f4a8f6465cd5bb5503c5
Lukas Gustafson <l_gustafson@fanshaweonline.ca>
2022-11-02 18:57:38 -0400

    UserInput & GameState updates.

--- NetworkExamples/UDPClient_CLI/udp_client_cli_main.cpp
+++ NetworkExamples/UDPClient_CLI/udp_client_cli_main.cpp
@@ -8,84 +8,181 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <conio.h>
+
 // Need to link Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
 #include <string>
+
+int g_Iteration = 0;
+
+struct PlayerInfo {
+	float x, y;
+};
+
+struct GameState {
+	PlayerInfo player;
+};
+
+struct UserInput {
+	bool W, A, S, D;
+};
+
+struct ServerInfo {
+	SOCKET socket;
+	sockaddr_in Addr;
+	int addressSize;
+};
+
+ServerInfo g_ServerInfo;
+UserInput g_UserInput;
+GameState g_GameState;
+
+bool g_UserSentInput;
+
+void SendUpdateToGameServer()
+{
+	char input[4];
+	input[0] = g_UserInput.W;
+	input[1] = g_UserInput.A;
+	input[2] = g_UserInput.S;
+	input[3] = g_UserInput.D;
+	int bufsize = sizeof(UserInput);
+
+	printf("%d Input: %d %d %d %d => ", g_Iteration++, g_UserInput.W, g_UserInput.A, g_UserInput.S, g_UserInput.D);
+	int sendResult = sendto(g_ServerInfo.socket, input, bufsize, 0, (SOCKADDR*)&g_ServerInfo.Addr, g_ServerInfo.addressSize);
+	if (sendResult == SOCKET_ERROR)
+	{
+		if (WSAGetLastError() != WSAEWOULDBLOCK)
+		{
+			printf("send failed with error: %d\n", WSAGetLastError());
+			closesocket(g_ServerInfo.socket);
+			WSACleanup();
+		}
+
+		return;
+	}
+}
+
+void CheckForUpdateFromGameServer()
+{
+	const int sizeofGameState = sizeof(GameState);
+	char buf[sizeofGameState];
+
+	int recvResult = recvfrom(g_ServerInfo.socket, buf, sizeofGameState, 0, (SOCKADDR*)&g_ServerInfo.Addr, &g_ServerInfo.addressSize);
+	if (recvResult == SOCKET_ERROR)
+	{
+		if (WSAGetLastError() != WSAEWOULDBLOCK)
+		{
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(g_ServerInfo.socket);
+			WSACleanup();
+		}
+
+		return;
+	}
+
+	memcpy(&g_GameState, (const void*)buf, sizeofGameState);
+
+	printf(" { %.2f, %.2f }\n", g_GameState.player.x, g_GameState.player.y);
+
+	int breakpoint = 0;
+}
+
+void NetworkUpdate()
+{
+	SendUpdateToGameServer();
+
+	CheckForUpdateFromGameServer();
+}
+
+bool g_DoQuit = false;
+
+void HandleUserInput()
+{
+	if (_kbhit())
+	{
+		char ch = _getch();
+		if (ch == 27)	// ESCAPE
+			g_DoQuit = true;
+
+		if (ch == 'w' || ch == 'W')
+			g_UserInput.W = true;
+		if (ch == 'a' || ch == 'A')
+			g_UserInput.A = true;
+		if (ch == 's' || ch == 'S')
+			g_UserInput.S = true;
+		if (ch == 'd' || ch == 'D')
+			g_UserInput.D = true;
+
+		g_UserSentInput = g_UserInput.W || g_UserInput.A || g_UserInput.S || g_UserInput.D;
+	}
+}
+
+void GameLoop()
+{
+	HandleUserInput();
+	NetworkUpdate();
+
+	// Clear user input for the next loop
+	g_UserInput.W = 0;
+	g_UserInput.A = 0;
+	g_UserInput.S = 0;
+	g_UserInput.D = 0;
+}
+
 
 int main(int argc, char** argv) {
 	// Initialization 
 	WSADATA wsaData;
 	int result;
 
-	printf("WSAStartup . . . ");
 	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (result != 0) {
 		printf("WSAStartup failed with error %d\n", result);
 		return 1;
 	}
 	else {
-		printf("Success!\n");
+		printf("WSAStartup: Success!\n");
 	}
 
 	// Create our UDP Socket
-	SOCKET serverSocket;
-	printf("Creating our Connection Socket . . . ");
-	serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (serverSocket == INVALID_SOCKET) {
+	g_ServerInfo.socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (g_ServerInfo.socket == INVALID_SOCKET) {
 		printf("socket failed with error: %d\n", WSAGetLastError());
 		WSACleanup();
 		return 1;
 	}
 	else {
-		printf("Success!\n");
+		printf("socket: Success!\n");
 	}
 
-	struct sockaddr_in SenderAddr;
-	int SenderAddrSize = sizeof(SenderAddr);
-	struct sockaddr_in serverAddr;
+	g_ServerInfo.Addr.sin_family = AF_INET;
+	g_ServerInfo.Addr.sin_port = htons(5555);
+	g_ServerInfo.Addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	g_ServerInfo.addressSize = sizeof(g_ServerInfo.Addr);
 
-	const int recvBufLen = 128;
-	char recvBuf[recvBufLen];
+	DWORD NonBlock = 1;
+	result = ioctlsocket(g_ServerInfo.socket, FIONBIO, &NonBlock);
+	if (result == SOCKET_ERROR) {
+		printf("ioctlsocket failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+	else {
+		printf("ioctlsocket: Success!\n");
+	}
 
-	const char* buf = "Ping";
-	int buflen = 4;
-
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(5555);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	// [send]
-	for (int i = 0; i < 3; i++)
+	g_UserSentInput = false;
+	g_DoQuit = false;
+	while (!g_DoQuit)
 	{
-
-		printf("Sending message to the server . . . ");
-		result = sendto(serverSocket, buf, buflen, 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-		if (result == SOCKET_ERROR) {
-			printf("send failed with error: %d\n", WSAGetLastError());
-			closesocket(serverSocket);
-			WSACleanup();
-			return 1;
-		}
-		else {
-			printf("Success!\n");
-			printf("Sent %d bytes to the server!\n", result);
-		}
-
-		result = recvfrom(serverSocket, recvBuf, recvBufLen, 0, (SOCKADDR*)&SenderAddr, &SenderAddrSize);
-		if (result == SOCKET_ERROR) {
-			printf("recv failed with error: %d\n", WSAGetLastError());
-			closesocket(serverSocket);
-			WSACleanup();
-			return 1;
-		}
-		else {
-			printf("Success!\n");
-			printf("recv %d bytes from the server: %s!\n", result, recvBuf);
-		}
+		GameLoop();
 	}
+
 	printf("Closing . . . \n");
-	closesocket(serverSocket);
+	closesocket(g_ServerInfo.socket);
 	WSACleanup();
 
 	return 0;
--- NetworkExamples/UDPServer_CLI/udp_server_cli_main.cpp
+++ NetworkExamples/UDPServer_CLI/udp_server_cli_main.cpp
@@ -8,11 +8,127 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include <conio.h>
 
 // Need to link Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
 #define DEFAULT_PORT "5555"
+
+struct PlayerInfo {
+	float x, y;
+};
+
+struct GameState {
+	PlayerInfo player;
+};
+
+struct ServerInfo {
+	SOCKET socket;
+};
+
+struct ClientInfo {
+	bool HaveInfo;
+	sockaddr_in clientAddr;
+	int clientAddrSize;
+};
+
+struct UserInput {
+	bool W, A, S, D;
+};
+
+int g_Iteration;
+GameState g_GameState;
+ClientInfo g_ClientInfo;
+UserInput g_UserInput;
+ServerInfo g_ServerInfo;
+bool g_doQuit = false;
+
+void GetInfoFromClients()
+{
+	const int bufsize = sizeof(UserInput);
+	char buf[bufsize];
+
+	int recvResult = recvfrom(g_ServerInfo.socket, buf, bufsize, 0, (SOCKADDR*)&g_ClientInfo.clientAddr, &g_ClientInfo.clientAddrSize);
+	if (recvResult == SOCKET_ERROR)
+	{
+		if (WSAGetLastError() != WSAEWOULDBLOCK)
+		{
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(g_ServerInfo.socket);
+			WSACleanup();
+			g_doQuit = true;
+		}
+
+		return;
+	}
+
+	g_ClientInfo.HaveInfo = true;
+	memcpy(&g_UserInput, (const void*)buf, bufsize);
+
+	printf("%d Input: %d %d %d %d => ", g_Iteration++, g_UserInput.W, g_UserInput.A, g_UserInput.S, g_UserInput.D);
+}
+
+void UpdateClientsWithGameState()
+{
+	if (g_ClientInfo.HaveInfo == false)
+		return;
+
+	int gameStateSize = sizeof(GameState);
+	printf(" { %.2f, %.2f }\n", g_GameState.player.x, g_GameState.player.y);
+	int sendResult = sendto(g_ServerInfo.socket, (const char*)&g_GameState, gameStateSize, 0, (SOCKADDR*)&g_ClientInfo.clientAddr, g_ClientInfo.clientAddrSize);
+	if (sendResult == SOCKET_ERROR)
+	{
+		if (WSAGetLastError() != WSAEWOULDBLOCK)
+		{
+			printf("send failed with error: %d\n", WSAGetLastError());
+			closesocket(g_ServerInfo.socket);
+			WSACleanup();
+			g_doQuit = true;
+		}
+
+		return;
+	}
+}
+
+void GameUpdate()
+{
+	// Update the game state
+	if (g_UserInput.A) {
+		g_GameState.player.x--;
+	}
+	if (g_UserInput.D) {
+		g_GameState.player.x++;
+	}
+	if (g_UserInput.W) {
+		g_GameState.player.y++;
+	}
+	if (g_UserInput.S) {
+		g_GameState.player.y--;
+	}
+}
+
+void GameLoop()
+{
+	GetInfoFromClients();
+
+	GameUpdate();
+
+	UpdateClientsWithGameState();
+}
+
+void HandleUserInput()
+{
+	if (_kbhit())
+	{
+		int ch = _getch();
+
+		if (ch == 27)
+		{
+			g_doQuit = true;
+		}
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -32,8 +148,8 @@ int main(int argc, char** argv)
 
 	// Create our socket [Socket]
 	printf("Creating our Listen Socket . . . ");
-	SOCKET listenSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (listenSocket == INVALID_SOCKET) {
+	g_ServerInfo.socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (g_ServerInfo.socket == INVALID_SOCKET) {
 		printf("Socket failed with error: %d\n", WSAGetLastError());
 		WSACleanup();
 		return 1;
@@ -42,13 +158,23 @@ int main(int argc, char** argv)
 		printf("socket: Success!\n");
 	}
 
-	struct sockaddr_in recvAddr;
+	DWORD NonBlock = 1;
+	result = ioctlsocket(g_ServerInfo.socket, FIONBIO, &NonBlock);
+	if (result != 0) {
+		wprintf(L"ioctlsocket failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+	else {
+		printf("ioctlsocket: Success!\n");
+	}
 
+	struct sockaddr_in recvAddr;
 	recvAddr.sin_family = AF_INET;
 	recvAddr.sin_port = htons(5555);
 	recvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	result = bind(listenSocket, (SOCKADDR*)&recvAddr, sizeof(recvAddr));
+	result = bind(g_ServerInfo.socket, (SOCKADDR*)&recvAddr, sizeof(recvAddr));
 	if (result != 0) {
 		wprintf(L"bind failed with error %d\n", WSAGetLastError());
 		WSACleanup();
@@ -58,54 +184,18 @@ int main(int argc, char** argv)
 		printf("bind: Success!\n");
 	}
 
-	const int buflen = 128;
-	char buf[buflen];
+	g_ClientInfo.clientAddrSize = sizeof(g_ClientInfo.clientAddr);
+	g_ClientInfo.HaveInfo = false;
 
-	// if successful, recv returns the number of bytes received
-	struct sockaddr_in SenderAddr;
-	int SenderAddrSize = sizeof(SenderAddr);
-
-	int count = 0;
-	while (count < 5)
+	while (!g_doQuit)
 	{
-		count++;
-
-		printf("recv . . . ");
-		result = recvfrom(listenSocket, buf, buflen, 0, (SOCKADDR*)&SenderAddr, &SenderAddrSize);
-		if (result == SOCKET_ERROR) {
-			printf("recv failed with error: %d\n", WSAGetLastError());
-			closesocket(listenSocket);
-			WSACleanup();
-			return 1;
-		}
-		else {
-			printf("Success!\n");
-			printf("Message From the client:\n%s\n", buf);
-
-			char ipAddress[INET_ADDRSTRLEN];
-			inet_ntop(AF_INET, &(SenderAddr.sin_addr), ipAddress, INET_ADDRSTRLEN);
-
-			printf(" Client Info: %s : %d\n", ipAddress, SenderAddr.sin_port);
-		}
-
-		std::string msg("Pong!");
-		result = sendto(listenSocket, msg.c_str(), 5, 0, (SOCKADDR*) & SenderAddr, SenderAddrSize);
-		if (result == SOCKET_ERROR) {
-			printf("send failed with error: %d\n", WSAGetLastError());
-			closesocket(listenSocket);
-			WSACleanup();
-			return 1;
-		}
-		else {
-			printf("Success!\n");
-			printf("Message sent to the client:\n%s\n", msg.c_str());
-		}
+		HandleUserInput();
+		GameLoop();
 	}
-
 
 	// Close
 	printf("Closing . . . \n");
-	closesocket(listenSocket);
+	closesocket(g_ServerInfo.socket);
 	WSACleanup();
 
 	return 0;
